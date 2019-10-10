85f0f83939ab347f57ab08e07830ae510ce0b865
jprous85 <jprous85@gmail.com>
2019-10-10 23:57:30 +0200

    auth + template

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('first_last_name');
-            $table->string('second_last_name');
+            $table->string('first_last_name')->nullable();
+            $table->string('second_last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('token');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
