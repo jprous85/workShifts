e2d442d39f658f9319c660730f663356f93222f4
jprous85 <jprous85@gmail.com>
2019-10-23 12:09:28 +0200

    Sidebar

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -1,16 +1,36 @@
 <div class="sidebar">
-    <div class="sidebar-title">
+    <div class="sidebar-title p-3">
         <a class="" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            {{ 'WorkShifts' }}
         </a>
     </div>
-    <div class="sidebar-item">
-        <a href="">{{ trans('service') }}</a>
-    </div>
-    <div class="sidebar-item">
-        <a href="">{{ trans('shedules') }}</a>
-    </div>
-    <div class="sidebar-item">
-        <a href="">{{ trans('workers') }}</a>
-    </div>
+
+    <a href="" class="link-sidebar">
+        <div class="sidebar-item p-3">
+            <i class="fas fa-clinic-medical"></i>
+            &nbsp;
+            {{ trans('service') }}
+        </div>
+    </a>
+    <a href="" class="link-sidebar">
+        <div class="sidebar-item p-3">
+            <i class="fas fa-clock"></i>
+            &nbsp;
+            {{ trans('shedules') }}
+        </div>
+    </a>
+    <a href="" class="link-sidebar">
+        <div class="sidebar-item p-3">
+            <i class="fas fa-map-marked-alt"></i>
+            &nbsp;
+            {{ trans('workplace') }}
+        </div>
+    </a>
+    <a href="" class="link-sidebar">
+        <div class="sidebar-item separator p-3">
+            <i class="fas fa-user-friends"></i>
+            &nbsp;
+            {{ trans('workers') }}
+        </div>
+    </a>
 </div>
